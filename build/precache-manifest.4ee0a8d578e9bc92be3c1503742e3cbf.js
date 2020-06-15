37bda339b8129b493adfa5a1baa49b1a2c6dc811
Abdullahhashmi06 <64926823+Abdullahhashmi06@users.noreply.github.com>
2020-06-15 22:43:15 +0500

    Project 1 Done

---
+++ build/precache-manifest.4ee0a8d578e9bc92be3c1503742e3cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace15ad9a0f10cd8cbc499d1f0742fde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87be3ac2f2cd27029b7d",
+    "url": "/static/css/main.c434d29e.chunk.css"
+  },
+  {
+    "revision": "132830fb04444d13e1a8",
+    "url": "/static/js/2.eb39c7fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb39c7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87be3ac2f2cd27029b7d",
+    "url": "/static/js/main.ffa61de7.chunk.js"
+  },
+  {
+    "revision": "0fb5aa10d11e0a3e8d13",
+    "url": "/static/js/runtime-main.56c06abf.js"
+  }
+]);
